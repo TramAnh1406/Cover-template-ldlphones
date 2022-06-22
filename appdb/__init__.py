0f57d196efe5bd59e46d3da222dedd6a1839b668
Tram Anh <anhltps11760@fpt.edu.vn>
2022-06-22 17:19:24 +0700

    update page code

--- appdb/__init__.py
+++ appdb/__init__.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('home/index.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('home/contact/contact.html')
--- appdb/static/js/slide.js
+++ appdb/static/js/slide.js
@@ -1,15 +1,23 @@
 var menu = ['IPhone 13 Series', 'Apple Watch', 'IPad Pro ', 'Macbook Pro', 'Phụ kiện Apple']
 var menuTitle = ['Hotsale giảm sốc', 'Mua nhiều giảm nhiều', 'Siêu sale giảm khủng', 'Đặt trước nhiều ưu đãi', 'Nhận quà cực khủng']
-var mySwiper = new Swiper ('.swiper-container', {
-    pagination: {
-      el: '.swiper-pagination',
-			clickable: true,
-        renderBullet: function (index, className) {
-          return '<span class="' + className + '">' + (menu[index]) + '<br>' + (menuTitle[index]) + '</span>';
-        },
+var mySwiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (menu[index]) + '<br>' + (menuTitle[index]) + '</span>';
     },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  })
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
+
+var swiper = new Swiper('.customerSwiper', {
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+})
