98d7b584a9b369fe821f07984789df4e1872eeba
Tram Anh <anhltps11760@fpt.edu.vn>
2022-06-24 09:45:57 +0700

    update code and deploy

--- appdb/__init__.py
+++ appdb/__init__.py
@@ -7,14 +7,27 @@ app = Flask(__name__)
 def home():
     return render_template('home/index.html')
 
+
 @app.route("/contact")
 def contact():
     return render_template('home/contact/contact.html')
+
 
 @app.route("/danh-muc")
 def category():
     return render_template('home/category/product.html')
 
+
 @app.route("/detail-product")
 def detailproduct():
     return render_template('home/category/product-detail.html')
+
+
+@app.route("/tintuc")
+def tintuc():
+    return render_template('home/tintuc/tintuc.html')
+
+
+@app.route("/chitiet-tintuc")
+def tintucdetail():
+    return render_template('home/tintuc/tintuc-detail.html')
