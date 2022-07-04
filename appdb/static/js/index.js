52aa606efd9587dfbd79c49789984c4c0198510f
Tram Anh <anhltps11760@fpt.edu.vn>
2022-07-04 09:00:24 +0700

    update

--- appdb/static/js/index.js
+++ appdb/static/js/index.js
@@ -1,9 +1,32 @@
+
+let btn = document.querySelector('.icon-scroll-top');
 let ele = document.querySelector("#cate-fixed");
-window.addEventListener("scroll", function(event) {
+window.addEventListener("scroll", function (event) {
 
     if (this.scrollY >= 400) {
         ele.classList.add('fixed');
     } else {
         ele.classList.remove("fixed");
     }
+});
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        $(btn).addClass('show');
+    } else {
+        $(btn).removeClass('show');
+    }
+});
+
+$(btn).on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
+$(".menu-open").on('click', function (e) {
+    e.preventDefault();
+    $(".icon-menu-mb").parent().toggleClass('active');
+});
+$(".menu-close").on('click', function (e) {
+    e.preventDefault();
+    $(".icon-menu-mb").parent().removeClass('active');
 });
